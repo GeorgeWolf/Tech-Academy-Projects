7eacd39c89d4a7b35178870c1a736d2f8cfecda2
George Wolf <georgewolf.ot@gmail.com>
2017-06-07 19:36:17 +0200

    First 2 Python games
    TA Python projects

---
+++ Python Practice/demo.py
@@ -0,0 +1,172 @@
+# -*- coding: cp1250 -*-
+# Python:   2.7.13
+#
+# Author:   George Wolf (georgewolf.ot@gmail.com)
+#
+# Purpose:  The Tech Academy - Python Course, Drill (p36)
+
+
+
+
+
+def start(health=100,attack=10,name="",title="The Wanderer"): # 1. Assign an integer to a variable / 2. Assign a string to a variable / 12. Define a function that returns a string variable
+    brag = float(3)/float(9) # 3. Assign a float to a variable
+    tuples = ('Orcs', 'Trolls', 'Dragons') # 11. Create a tuple and iterate through it using a for loop to print each item out on a new line
+    lists = [7, 13, 9, 19, 3]
+    name = game_intro(name,brag,tuples,lists)
+    health,attack,name,title = character_stats(health,attack,name,title)
+
+
+def game_intro(name,brag,tuples,lists):
+    if name != "": # If name is already given
+        print("\n\n\n Welcome back {}!".format(name)) # 4. Use the print function and .format() notation to print out the variable you assigned
+    else:
+        change = True
+        while change:
+            if name == "":
+                name = raw_input("\nHow do we call you stranger? ").capitalize()
+                if name != "":
+                    print("\n\nGreetings {}!".format(name))
+                    print("\nI see you have just awakened. Let me introduce myself. My name is Merlin. Yes, that Merlin.")                
+                    print("\nI don't want to brag, but I even know how to divide, like 3/9!")
+                    print("\nI see you are confused, so I tell you. 3/9 = {}. See? Pretty simple.").format(brag)
+                    print("\n\nYou are talking too much, there is no time to waste, The kingdom is in great danger, you must come with me!")
+                    print("\nThe wizard is mumbling about creatures of this land, like:")
+                    for countT in tuples: # 11. Create a tuple and iterate through it using a for loop to print each item out on a new line
+                        print countT
+                    print("\nHe looks at you still mumbling some numbers which don't make any sense.")
+                    for countL in lists: # 9. Use of a for loop / 10. Create a list and iterate through that list using a for loop to print each item out on a new line
+                        print countL
+                    print("\nMaybe these are the lucky numbers for the coming lottery. Who knows?")
+                    print("\nThen suddenly he turns to you and says:")
+                    print("\n'It's dangerous to go alone! Take this.'")
+                    print("\n\n             RUSTY SWORD (Attack: 10)")
+                    print("\n\nAnd he suddenly disappears.")
+                    print("\nYou decide to get moving.")
+                    change = False
+                
+    return name
+
+
+def character_stats(health,attack,name,title):
+    change = True
+    while change:
+        show_stats(health,attack,name,title)
+        
+        pick = raw_input("which way to go? West(w), North(n), East(e), South(s): ").lower()
+        if pick == "w" and health > 99 and attack < 16: # 6. Use of logical operators: and, or, not
+            pick = raw_input("\n\nAs you wander in the wilderness a Cave Troll appears in the distance. This must be the creature Merlin mumbled about. It looks huge and robust, dragging a club after him. What to you do? Attack(a)/Flee(f): ").lower()
+            if pick == "a":
+                print("\n\nYou have fiercely attacked the Troll. His thick skin is nearly impenetrable. He kicks you aside and slowly moves on. \nYou barely escape. Luckily he was too bored to go after you. \nYour health is halved.")
+                health = health/2 # 5. Use each of these operators
+                change = not True
+            if pick == "f":
+                print("\n\nA great warrior knows when to attack and when to flee. \nThis might be the right choice for the second one. \nAs you walk away, you spend the rest of the day sharpening your sword. \nYou have just gained +2 Attack!")
+                attack += 2 # 5. Use each of these operators
+                change = False
+        if pick == "w" and health < 100 and attack < 16:
+            print("\n\nYou hear a voice in your head: 'You are running to your death'")
+            pick = raw_input("\n\nAs you wander in the wilderness a Cave Troll appears in the distance. This must be the creature Merlin mumbled about. It looks huge and robust, dragging a club after him. What to you do? Attack(a)/Flee(f): ").lower()
+            if pick == "a":
+                print("\n\nYou have fiercely attacked the Troll. His thick skin is nearly impenetrable. He kicks you aside and slowly moves on. \nLuckily he was too bored to beat you to death. \nYour health is halved.")
+                health = health/2 # 5. Use each of these operators
+                change = False
+            if pick == "f":
+                print("\n\nA great warrior knows when to attack and when to flee. \nThis might be the right choice for the second one. \nAs you walk away, you spend the rest of the day sharpening your sword. \nYou have just gained +2 Attack!")
+                attack += 2 # 5. Use each of these operators
+                change = False
+        if pick == "w" and attack > 15:
+            print("\nYou charge with all your power. With only one slice you defeat the Cave Troll, you feel the energy going through your body, your sword starts to glow. \nAttack increased: +5")
+            attack = attack +5
+            win(health,attack,name,title)
+        if pick == "n" and attack < 16:
+            print("\nYou hear whispering in your mind: 'You are not ready yet!' \nYou have to go elsewhere.")
+            change = False
+        if pick == "n" and attack > 16:
+            print("\nYou are now ready to face your destiny! (In the next episode!)")
+        if pick == "e" and health > 99:
+            print("\nYou have an urge to go West!")
+            change = False
+        if pick == "e" and health < 100 and health > 2:
+            print("\nYou find a Fountain with glowing water. You already feel better as you get closer to the Fountain. \nAs you drink from it, your life starts to regenerate.\n\nLife doubled.")
+            health = health * 2
+            change = False
+        if pick == "e" and health == 2 or pick == "e" and health == 1: # 6. Use of logical operators: and, or, not
+            print("\nYou find a Fountain with glowing water. You already feel better as you get closer to the Fountain. \nAs you get closer the Fountain starts to talk:\n\n'Damn son! You look like s#@t! I think you need some serious help here!'\n\nThe fountain heals you to full health. Life restored.")
+            health = 100
+            change = False
+        if pick == "s":
+            print("\nNever go back! Basically this is where you came from (Or maybe from Oregon :P).")
+            print("\nYou lose nearly all of your life.")
+            health = health%health # Resets life to 0
+            health = health -1 # 5. Use each of these operators
+            health = health +3 # Just having some fun with numbers :D
+            change = False
+        if attack == 12:
+            print("\n\nYou are on the right track. Just keep it up!")
+        if attack == 14:
+            print("\n\nYou hear a voice in your head: 'That's the spirit! I mean for the weapon upgrade, not for the running.'")
+        if attack == 16:
+            print("\n\nYou have done it! Now go to West and defeat the Troll!")
+        stats(health,attack,name,title)
+
+
+def show_stats(health,attack,name,title):
+    print("\n\nStats of {}:").format(name)
+    print("\nTitle: {}").format(title)
+    print("\nHealth: {}/100, Attack: {}").format(health,attack)
+    print("\n\n               ---------*---------")
+    
+
+def stats(health,attack,name,title):
+    if health < 1:
+        lose(health,attack,name,title)
+    else:
+        character_stats(health,attack,name,title)
+
+
+def win(health,attack,name,title):
+    print("\n\n\nYou are ready {}! Merlin smiles in the distance as he watches you triumph. \nThank you for playing! More coming in the future ;P").format(name)
+    again(health,attack,name,title)
+
+
+def lose(health,attack,name,title):
+    print("\n\n\nAs you fall to the ground you can see the face of Merlin in the distance. You feel a voice inside of your head: '{}, get up and fight!'").format(name)
+    again(health,attack,name,title)
+
+
+def again(health,attack,name,title):
+    change = True
+    while change: # 7. Use of conditional statements: if, elif, else / 8. Use of a while loop
+        choice = raw_input("\nDo you wish to Continue(c)?/ Start a New Game(n)?/ Or Exit(x)? ").lower()
+        if choice == "c": # Continue
+            print("\nYou recover and your journey continues.") 
+            change = False
+            resetContinue(health,attack,name,title)
+        elif choice == "n": # Reset/New Game with the same name
+            change = False
+            reset(health,attack,name,title)
+        elif choice == "x": # Exit
+            print("\nEven the greatest warriors need some rest.") 
+            change = False
+            exit()
+        else:
+            print("\n\n\nPlease enter 'c' for 'Continue the Game', 'n' for 'New Game', 'x' for 'Exit the Game'...")
+
+
+def resetContinue(health,attack,name,title): # Sets values when continue is choosen
+    health = 50
+    attack = attack
+    title = "The Survivor"
+    start(health,attack,name,title)
+
+
+def reset(health,attack,name,title): # Sets values when reset is choosen
+    health = 100
+    attack = 10
+    title = "The Reborn"
+    start(health,attack,name,title)
+
+
+if __name__ == "__main__": # 13. Call the function you defined above and print the result to the shell
+    start()
---
+++ Python Practice/nice_mean.py
@@ -0,0 +1,133 @@
+# Python:   2.7.13
+#
+# Author:   George Wolf (georgewolf.ot@gmail.com)
+#
+# Purpose:  The Tech Academy - Python Course, Drill 6 of 63
+#           Demonstrating how to pass variables from function to function
+#           while producing a functional game.
+#
+#           Remember, function_name(variable) _means that we pass in the variable.
+#           return variable _means that we are returning the variable to
+#           the calling function
+# 1.
+# def start():
+#     print(get_number())
+#
+# def get_number():
+#     number = 12
+#     return number
+#
+# 2.
+# def start():
+#     print("Hello {}!".format(get_name()))
+#
+# def get_name():
+#     name = raw_input("What is your name?")
+#     return name
+#
+# 3.
+# def start():
+#     f_name = "Sarah"
+#     l_name = "Connor"
+#     age = 28
+#     gender = "Female"
+#     get_info(f_name,l_name,age,gender)
+#
+# def get_info(f_name,l_name,age,gender):
+#     print("My name is {0} {1}. I am {2} yearold {3}.".format(f_name,l_name,age,gender))
+
+
+def start(nice=0,mean=0,name=""):
+    # Get user's name
+    name = describe_game(name)
+    nice,mean,name = nice_mean(nice,mean,name)
+
+
+def describe_game(name):
+    """
+        check if this is a new game or not.
+        If it is new, get the user's name.
+        If it is not a new game, thank the player for
+        playing again and continue with the game
+    """
+    if name != "": # Meaning, if we do not already have this user's name, then they are a new player and we need to get their name
+        print("\n\n\nThank you for plyaing again, {}!".format(name))
+    else:
+        stop = True
+        while stop:
+                if name == "":
+                    name = raw_input("\nWhat is your name?").capitalize()
+                    if name != "":
+                        print("\nWelcome, {}!".format(name))
+                        print("\nIn this game, you will be greeted by several different clan leaders. \nYou can be nice or mean.")
+                        print("\nAt the end of the game your fate will be influenced from your actions.")
+                        print("The future of your clan depends on you! Good luck warchief!")
+                        stop = False
+    return name
+
+
+def nice_mean(nice,mean,name):
+    stop = True
+    while stop:
+        show_score(nice,mean,name)
+        pick = raw_input("\n\n\nA warrior from a stranger clan approaches you. He is wielding a battle axe\nWill you be nice or mean? n/m:").lower()
+        if pick == "n":
+            print("\n\n\nHe greets you with respect. Your ways are parting, but you know you have just made a friend...")
+            nice = (nice +1)
+            stop = False
+        if pick == "m":
+            print("\n\n\nThe warrior glares at you menacingly. He holds his axe strong, but decides not to engage today. He will remember though. You just made an enemy...")
+            mean = (mean +1)
+            stop = False
+        score(nice,mean,name) # Pass the 3 variables to the score ()
+
+
+def show_score(nice,mean,name):
+    print("\n{}, you currently have({}, Nice) and ({}, Mean) karma points.".format(name,nice,mean))
+
+
+def score(nice,mean,name):
+    # Score function is being passed the values stored within the 3 variables
+    if nice > 3: # If condition is valid, call win function passing in the variables so it can use them
+        win(nice,mean,name)
+    if mean > 3: # If condition is valid, call lose function passing in the variables so it can use them
+        lose(nice,mean,name)
+    else:        # Else, call nice_mean function passing in the variables so it can use them
+        nice_mean(nice,mean,name)
+
+
+def win(nice,mean,name):
+    print("\n\n\nGreat job warchief {}, you win! \nMajority of the clans loves you and will join your endeavour under your command! \nFor the warchief!".format(name)) # Substitute the {} wildcards with our variables
+    print("\nLong live the Queen... Uhhhh, I mean {}!!!".format(name))
+    again(nice,mean,name) # Call again function and pass in our variables
+
+
+def lose(nice,mean,name):
+    print("\n\n\nYour camp is in ruins, you lie on the ground as the ones who didn't got the greetings they expected stomp on your broken body. Your clan is at war.\n{}, you have failed your people!\nGame over!".format(name)) # Substitue the {} wildcards with our variables
+    again(nice,mean,name) # Call again function and pass in our variables
+
+
+def again(nice,mean,name):
+    stop = True
+    while stop:
+        choice = raw_input("\nDo you want to play again? y/n: ").lower()
+        if choice == "y":
+            stop = False
+            reset(nice,mean,name)
+        if choice == "n":
+            print("\nSee you later alligator!")
+            stop = False
+            exit()
+        else:
+            print("\nPlease enter 'y' for 'YES', 'n' for 'NO'...")
+            
+
+def reset(nice,mean,name):
+    nice = 0
+    mean = 0
+    # Notice, I do not reset the name variable as that same user has elected to play again
+    start(nice,mean,name)
+
+
+if __name__ == "__main__":
+    start()
