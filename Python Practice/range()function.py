5a46fb77ecfd3d2661193e8804935823ca08e47a
George Wolf <georgewolf.ot@gmail.com>
2017-06-15 14:02:21 +0200

    Tech Academy Drills added
    Some modification of demo added as wnes
    range()funtion drill added

---
+++ Python Practice/range()function.py
@@ -0,0 +1,19 @@
+# Python:   3.6.1
+#
+# Author:   George Wolf (georgewolf.ot@gmail.com)
+#
+# Purpose:  The Tech Academy - Python Course
+#           Python range() funtion Drill
+
+for i in range(4): # 1st drill
+    print(i)
+
+print("-----")
+
+for i in range(3,-1,-1): # 2nd drill
+    print(i)
+
+print("-----")
+
+for i in range(8,0,-2): # 3rd drill
+    print(i)
