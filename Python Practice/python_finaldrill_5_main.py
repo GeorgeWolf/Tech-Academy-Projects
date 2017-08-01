ee32eae29e73364de0be98c4517d7df0614fdd31
George Wolf <georgewolf.ot@gmail.com>
2017-08-01 19:24:00 +0200

    Added Python Final Drill 5
    Python Final Drill 5, added 3 files

---
+++ Python Practice/python_finaldrill_5_main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Python Ver:   3.6.2
+#
+# Author:       George Wolf (georgewolf.ot@gmail.com)
+#
+# Purpose:      Python Final Drill 5 - Database Functionality for File Transfer project (The Tech Academy)
+#
+# Tested OS:    This code was written and tested to work with Windows 10.
+
+
+from tkinter import *
+import tkinter as tk
+
+import python_finaldrill_5_gui
+import python_finaldrill_5_func
+
+# Frame is the Tkinter frame class that our own class will inherit from
+class ParentWindow(Frame):
+    def __init__(self, master, *args, **kwargs):
+        Frame.__init__(self, master, *args, **kwargs)
+
+        # define our master frame configuration
+        self.master = master
+        self.master.minsize(320,180) #(Height, Width)
+        self.master.maxsize(320,180)
+        # This CenterWindow method will center our app on the user's screen
+        python_finaldrill_5_func.center_window(self,500,300)
+        self.master.title("File Transfer")
+        self.master.configure(bg="#F0F0F0")
+        # This protocol method is a tkinter built-in method to catch if 
+        # the user clicks the upper corner, "X" on Windows OS.
+        self.master.protocol("WM_DELETE_WINDOW", lambda: python_finaldrill_5_func.ask_quit(self))
+
+        # load in the GUI widgets from a separate module, 
+        # keeping your code compartmentalized and clutter free
+        python_finaldrill_5_gui.load_gui(self)
+
+        
+if __name__ == "__main__":
+    root = tk.Tk()
+    App = ParentWindow(root)
+    root.mainloop()
