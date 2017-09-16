98478ceace09c6a4ed4fabf594da9f8bbadbffb3
George Wolf <georgewolf.ot@gmail.com>
2017-09-16 16:30:38 +0200

    TestApp for C#
    A simple C# application.
    Compiling TestApp.cs into a console application named TestApp.exe (3
    ways)

---
+++ C# Practice/TestApp.cs
@@ -0,0 +1,16 @@
+// A simple C# application.
+using System;
+
+class TestApp
+{
+	static void Main()
+	{
+		Console.WriteLine("Testing! 1, 2, 3");
+	}
+}
+
+// Compiling TestApp.cs into a console application named TestApp.exe (3 ways)
+
+// csc /target:exe TestApp.cs
+// csc /t:exe TestApp.cs
+// csc TestApp.cs
