20b96d7c3b57d514fd0e6f53c64a96f3369ff7c2
George Wolf <georgewolf.ot@gmail.com>
2017-11-27 17:02:39 +0100

    Lesson 7 Projects added
    Lesson 7 Projects and Challenges added

---
+++ C#,ASP.NET/Challenges/ChallengePostalCalculatorHelperMethodsDRY/ChallengePostalCalculatorHelperMethodsDRY/Default.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePostalCalculatorHelperMethodsDRY
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            resultLabel.Text = "";
+        }
+
+        protected void handleChange(object sender, EventArgs e)
+        {
+            performCalculation();
+        }
+
+        private void performCalculation()
+        {
+            // Check for the values in the TextBoxes & CheckBoxes
+            if (!valuesExist()) return;
+
+            // What is the volume
+            double volume = 0.0;
+            if (!tryGetVolume(out volume)) return;
+
+            // What is the multiplier
+            double postageMultiplier = getPostageMultiplier();
+
+            // Determine the cost
+            double cost = volume * postageMultiplier;
+
+            // Show the result
+            resultLabel.Text = String.Format("Your parcel will cost {0:C} to ship.", cost);
+        }
+
+        private bool valuesExist()
+        {
+            if (!airRadioButton.Checked
+                && !groundRadioButton.Checked
+                && !nextDayRadioButton.Checked)
+                return false;
+
+            if (widthTextBox.Text.Trim().Length == 0
+                || heightTextBox.Text.Trim().Length == 0)
+                return false;
+
+            return true;
+        }
+
+        private bool tryGetVolume(out double volume)
+        {
+            volume = 0.0;
+            double width = 0.0;
+            double height = 0.0;
+            double length = 0.0;
+
+            if (!double.TryParse(widthTextBox.Text.Trim(), out width)) return false;
+            if (!double.TryParse(heightTextBox.Text.Trim(), out height)) return false;
+            if (!double.TryParse(lengthTextBox.Text.Trim(), out length)) length = 1.0;
+
+            volume = width * height * length;
+            return true;
+        }
+
+        private double getPostageMultiplier()
+        {
+            if (groundRadioButton.Checked) return .15;
+            else if (airRadioButton.Checked) return .25;
+            else if (nextDayRadioButton.Checked) return .45;
+            else return 0;
+        }
+    }
+}
---
+++ C#,ASP.NET/Lesson 7/ChallengePostalCalculatorHelperMethods/ChallengePostalCalculatorHelperMethods/Default.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePostageCalculatorHelperMethods
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            resultLabel.Text = "";
+        }
+
+        protected void widthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void heightTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void lengthTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void groundRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            // Ground: .15 multiplier
+            calculatePostalPrice(.15);
+            resultText();
+        }
+
+        protected void airRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            // Air: .25 multiplier
+            calculatePostalPrice(.25);
+            resultText();
+        }
+
+        protected void nextDayRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            // Next Day: .45 multiplier
+            calculatePostalPrice(.45);
+            resultText();
+        }
+
+        private double calculatePackageSize()
+        {
+            double sizeOfPackage = 0.0;
+            double sizeOfWidth = 0.0;
+            double sizeOfHeight = 0.0;
+            double sizeOfLength = 0.0;
+            if (!Double.TryParse(widthTextBox.Text, out sizeOfWidth))
+                return sizeOfPackage;
+
+            if (!Double.TryParse(heightTextBox.Text, out sizeOfHeight))
+                return sizeOfPackage;
+
+            if (!Double.TryParse(lengthTextBox.Text, out sizeOfLength))
+                sizeOfLength = 1.0;
+
+            sizeOfPackage = sizeOfWidth * sizeOfHeight * sizeOfLength;
+
+            return sizeOfPackage;
+        }
+
+        private void calculatePostalPrice(double multiplyPackageSize)
+        {
+            double priceOfPackage = calculatePackageSize() * multiplyPackageSize;
+
+            resultLabel.Text = String.Format("Your parcel will cost {0:C} to ship.", priceOfPackage);
+        }
+        
+        private void resultText()
+        {
+            if ((widthTextBox.Text.Trim() == "0")
+                || (heightTextBox.Text.Trim() == "0")
+                || (lengthTextBox.Text.Trim() == "0"))
+                resultLabel.Text = "You have to provide a value other than 0.";
+            
+            else if (!(widthTextBox.Text.Trim().Length == 0) 
+                && !(heightTextBox.Text.Trim().Length == 0)) { }
+
+            else
+                resultLabel.Text = "You must provide both the Width and the Height of the Item to be shipped.";
+        }
+    }
+}
