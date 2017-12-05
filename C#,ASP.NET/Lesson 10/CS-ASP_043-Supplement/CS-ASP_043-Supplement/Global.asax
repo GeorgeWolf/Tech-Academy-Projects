379d4108678bbbae840aee3a48032eb6561d087d
George Wolf <georgewolf.ot@gmail.com>
2017-12-05 12:51:46 +0100

    Lesson 10 & Challenges Added

---
+++ C#,ASP.NET/Lesson 10/CS-ASP_043-Supplement/CS-ASP_043-Supplement/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CS_ASP_043_Supplement.Global" Language="C#" %>
---
+++ C#,ASP.NET/Lesson 10/ChallengeSimpleDarts/ChallengeSimpleDarts/Score.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Darts;
+
+namespace ChallengeSimpleDarts
+{
+    public class Score
+    {
+        public static void ScoreDart(Player player, Dart dart)
+        {
+            doubleTriple(player, dart);
+            bullsEye(player, dart);
+        }
+
+        private static void doubleTriple(Player player, Dart dart)
+        {
+            int score = 0;
+
+            if (dart.IsDouble) score = dart.Score * 2;
+            else if (dart.IsTriple) score = dart.Score * 3;
+
+            player.Score += score;
+        }
+
+        private static void bullsEye(Player player, Dart dart)
+        {
+            int score = 0;
+
+            if (dart.IsInner && dart.Score == 0) score = 50;
+            else if (dart.Score == 0) score = 25;
+
+            player.Score += score;
+        }
+    }
+}
