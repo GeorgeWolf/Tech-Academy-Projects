36bdce34b5599832b6bff605dcdfc1e9541a7b95
George Wolf <georgewolf.ot@gmail.com>
2017-12-19 14:00:45 +0100

    MVC projects added

---
+++ C#,ASP.NET/Challenges/MyComicBooks/MyComicBooks/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using MyComicBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyComicBooks.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var comics = ComicBookManager.GetComicBooks();
+            return View(comics);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var comics = ComicBookManager.GetComicBooks();
+            var comic = comics.FirstOrDefault(p => p.ComicBookId == id);
+            return View(comic);
+        }
+    }
+}
---
+++ C#,ASP.NET/MVC/MyComicBooksSolution/MyComicBooksSolution/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyComicBooksSolution.MvcApplication" Language="C#" %>
